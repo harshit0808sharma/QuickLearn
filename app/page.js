a0bc02be52c39dbe544744b421c8333cb5b005b7
sachin sharma <harshit0808sharma@gmail.com>
2025-08-02 17:51:35 +0530

    some extra commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-// import Navbar from '../components/layout/Navbar'
-
 import Hero from "./components/homecarts/Hero";
 import OurCourses from "./components/homecarts/OurCourses";
 import { OurServices } from "./components/homecarts/OurServices";
@@ -7,18 +5,20 @@ import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Review from "./components/other/Review";
 
-
 export default function Home() {
-    return (
-        <>
-            <div>
-                <Navbar />
-                <Hero />
-                <OurCourses />
-                <Review />
-                <OurServices />
-                <Footer />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+
+      {/* Content wrapper with top padding to prevent overlap with fixed navbar */}
+      <main className="pt-24 px-4 sm:px-6 md:px-8 overflow-x-hidden bg-gray-900">
+        <Hero />
+        <OurCourses />
+        <Review />
+        <OurServices />
+      </main>
+
+      <Footer />
+    </>
+  );
 }
